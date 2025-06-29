2e70e38eea8aadb5e3bdfb968ae44cb47cfdcec9
Thike <89530251+Thike02@users.noreply.github.com>
2025-06-30 01:23:11 +0900

    add github llink

--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -10,6 +10,7 @@ export default function MyApp(){
   return (
     <div>
       <h1>Welcome to My App</h1>
+      <a href="https://github.com/Thike02" target="_blank" rel="noopener noreferrer">ﾘﾝｸﾀﾞﾖ</a>
       <MyButton />
     </div>
   );
